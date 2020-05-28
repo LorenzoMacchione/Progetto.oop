8d9f16104f62ede8abe30e2ea3416b23aaf11e54
Lorenzo Macchione <S1087452@studenti.univpm.it>
2020-05-28 23:02:35 +0200

    Primi test su importo e restituzione di dati json

--- src/main/java/it/univpm/twitterProject/controller/simpleRestController.java
+++ src/main/java/it/univpm/twitterProject/controller/simpleRestController.java
@@ -2,18 +2,23 @@ package it.univpm.twitterProject.controller;
 
 import java.util.ArrayList;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
+import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import it.univpm.twitterProject.model.StartClass;
 import it.univpm.twitterProject.model.Tweet;
+
+import it.univpm.twitterProject.model.StartClass;
 
 @RestController
 public class simpleRestController {
 	
-	@RequestMapping(value = "start", method = RequestMethod.GET)
-	public ArrayList<Tweet> getMetadata(){
-			return StartClass.getTweets();
-		}	
+ @GetMapping("/getMetadata")
+ public String get() {
+	 //ArrayList<Tweet> c = new ArrayList<Tweet>();
+	 String c = StartClass.fillTweets();	 
+	//c=StartClass.getTweets();
+	 return c;
+ }
 }
--- src/main/java/it/univpm/twitterProject/model/StartClass.java
+++ src/main/java/it/univpm/twitterProject/model/StartClass.java
@@ -1,13 +1,62 @@
 package it.univpm.twitterProject.model;
 
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
 
 public class StartClass {
 	
 	private static  ArrayList<Tweet> tweets = new ArrayList<Tweet>();
 	
+	
+	
+	
+	public static String fillTweets () {
+		String data = "";
+		try {
+		String url = "https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/api/1.1/search/tweets.json?q=terremoto&count=100"; 
+		String fileName = "";
+		URLConnection openConnection = new URL(url).openConnection();
+		InputStream in = openConnection.getInputStream();
+		
+		 
+		 String line = "";
+		 try {
+		   InputStreamReader inR = new InputStreamReader( in );
+		   BufferedReader buf = new BufferedReader( inR );
+		  
+		   while ( ( line = buf.readLine() ) != null ) {
+			   data+= line;
+		   }
+		 } finally {
+		   in.close();
+		 }
+		System.out.println( data );
+		
+		JSONObject obj = (JSONObject) JSONValue.parseWithException(data);	 //parse JSON Object
+		System.out.println( "OK" );
+		
+		
+		}
+		catch (Exception e) {}
+		return data;
+		
+	}
+	
 	public static ArrayList<Tweet> getTweets() {
-		tweets.add(new Tweet("id", "testo", "località"));
 		return tweets;
 	}
 	
