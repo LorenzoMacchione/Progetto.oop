8ae9a61676610b1d0091d7c6ed535a4bee20e86a
Lorenzo Macchione <S1087452@studenti.univpm.it>
2020-06-13 10:01:00 +0200

    Migliorata distribuzione dei tweet con geo csuale

--- src/main/java/it/univpm/twitterProject/service/parser/parserTweet.java
+++ src/main/java/it/univpm/twitterProject/service/parser/parserTweet.java
@@ -38,8 +38,37 @@ public class parserTweet extends parser {
 				tweetsList.add(tweet);
 
 			} else {
-				Double a1 = gen.nextDouble() * 10 + 36;
-				Double a2 = gen.nextDouble() * 9 + 7;
+				Double a2=36.7; 
+				Double a1 = gen.nextDouble() * 10 + 36.7;
+				if (a1<38.2) {
+					a2 = gen.nextDouble() * 3 + 12.5;
+					}else {
+						if(a1<40.6) {
+							if(gen.nextInt(10)<3) {
+								a2 = gen.nextDouble() * 1 + 8.46;
+							}else {
+								a2 = gen.nextDouble() * 3 + 15.5;
+							}
+						}else {
+							if(a1<41.2) {
+								if(gen.nextInt(10)<3) {
+									a2 = gen.nextDouble() * 1 + 8.46;
+								}else {
+									a2 = gen.nextDouble() * 4 + 12.9;
+								}
+							}else {
+								if (a1<43.6) {
+									a2 = gen.nextDouble() * 4 + 10.9;
+								}else {
+									if (a1<44.5) {
+										a2 = gen.nextDouble() * 4 + 8.6;
+									}else {
+										a2 = gen.nextDouble() * 7 + 6.8;
+									}
+								}
+							}
+						}
+					}
 				tweet.setGeo(a1, a2);
 				tweetsList.add(tweet);
 
